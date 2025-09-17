b3169b1712960d0d20d18fe0d4843f6d8439c498
Zenny Wettersten <zenny.wettersten@cern.ch>
2025-09-17 13:15:00 +0200

    process-specific csv formatting

--- madgraph/interface/reweight_interface.py
+++ madgraph/interface/reweight_interface.py
@@ -1410,8 +1410,12 @@ class ReweightInterface(extended_cmd.Cmd):
             return
         self.exitted = True
         self.run_time = time.time() - self.run_time
-        with (open(os.path.join(self.me_dir, '..', 'rwgt_times.txt'), 'a')) as file:
-            file.write('[%s,%s,%s,%s]\n' %(self.launches, self.nb_events, self.compile_time, self.run_time) )
+        filename = "rwgt_times.txt"
+        if not os.path.exists(filename):
+            with open(os.path.join(self.me_dir,  'rwgt_times.txt'), 'w') as file:
+                file.write('iter, events, compile, run\n')
+        with open(os.path.join(self.me_dir, 'rwgt_times.txt'), 'a') as file:
+            file.write('%s,%s,%s,%s\n' %(self.launches, self.nb_events, self.compile_time, self.run_time) )
         if 'init' in self.banner:
             cross = 0 
             error = 0
